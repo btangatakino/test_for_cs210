73b6b701f349fe1256feb602380146d9f9ba2579
Ben George <93751310+btangatakino@users.noreply.github.com>
2023-04-29 19:25:47 -1000

    added abstraction/abs1.cs and .vscode/launch.json

---
+++ abtraction/abs1.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Employee
+{
+    public int EmployeeId;
+    public string EmployeeName;
+    public double GrossPay;
+    double TaxDeduction = 0.1;
+    double netSalary;
+
+    public Employee(int Eid, String Ename, double EGrossPay)
+    {
+        this.EmployeeId = Eid;
+        this.EmployeeName = Ename;
+        this.GrossPay = EGrossPay;
+    }
+
+    void CalculateSalary()
+    {
+        if(GrossPay >= 30000)
+        {
+            netSalary = GrossPay - (TaxDeduction * GrossPay);
+            Console.WriteLine("Your Salary is {0}", netSalary);
+        }
+        else
+        {
+            Console.WriteLine("Your Salary is {0}", GrossPay);
+        }
+    }
+    public void ShowEmployeeDetails()
+    {
+        Console.WriteLine("Employee Id is: {0}",this.Eid);
+        Console.WriteLine("Employee Name is: {0}",this.Ename);
+        this.CalculateSalary();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Employee Ali = new Employee(333,"Ali Khan",40000);
+        Ali.ShowEmployeeDetails();
+        Console.ReadLine();
+    }
+}
